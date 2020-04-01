ff77ccf1acc8f8b75596a625a069d530bfa646d0
spotpush <sjhardy@btopenworld.com>
2020-04-01 14:10:12 +0100

    new local repo sync

---
+++ HeadfirstJava/Party.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party{
+	public void buildinvite() {
+		Frame f = new Frame();
+		Lable l = new Lable("Party at Tim's");
+		Button b = new Button("You bet");
+		Button c = new Button("Shoot me");
+		Panel p =new Panel();
+		p.add();
+	}
+}
