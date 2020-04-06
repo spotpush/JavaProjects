18319c7d920b9cd9488fb8c9162bc8db6f4b0704
spotpush <60810353+spotpush@users.noreply.github.com>
2020-04-06 18:01:03 +0100

    JSP, JSF, DAO Design Pattern

---
+++ JavaServerPages/src/java/com/spotpush/JavaServerPages/ControllerServlet.java
@@ -0,0 +1,15 @@
+/**
+
+*/
+/** @author Stephen Hardy @ spotpush.com  */
+package com.spotpush.JavaServerPages;
+
+/*Imports*/
+
+public class ControllerServlet 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
